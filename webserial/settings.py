dc4473cd2351d5545c8c49a53c68499151535bf5
David Hayden <dhayden@talosthoren.com>
2025-08-19 21:50:10 -0300

    Serving static files.

--- webserial/settings.py
+++ webserial/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT='static/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'sercon/node_modules',
+    BASE_DIR / 'sercon/node_modules/@xterm/xterm',
 ]
 
 # Default primary key field type
